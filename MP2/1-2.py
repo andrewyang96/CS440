0a4d10391aa30007b973fc5798a9f8aed3abefd6
PeopleKnowMe <sanil.pruthi@gmail.com>
2015-10-23 02:35:12 -0500

    still working on grid connections

--- MP2/1-2.py
+++ MP2/1-2.py
@@ -1,13 +1,21 @@
 import random
+import sys
 
-def init_square(grid):	
-	for i in range(0,10):
-		for j in range(0,10):
-			x = 10*random.random()
-			if(x > 7):
-				grid[i][j] = 'N'
-			else:
-				grid[i][j] = 0
+def init_square(grid, N):	
+	while(N > 0):
+		a = int(10*random.random())
+		b = int(10*random.random())
+		if(grid[a][b] != 'N'):
+			grid[a][b] = 'N'
+			N = N - 1
+	# for i in range(0,10):
+	# 	for j in range(0,10):
+	# 		x = 10*random.random()
+	# 		if(x > 7 and N > 0):
+	# 			grid[i][j] = 'N'
+	# 			N = N - 1
+	# 		else:
+	# 			grid[i][j] = 0
 
 
 def print_grid(grid):
@@ -28,5 +36,6 @@ def print_grid(grid):
 
 #grid = grid(10,10,0,0)
 grid = [[0 for x in range(10)] for x in range(10)]
-init_square(grid)
+init_square(grid, sys.argv[1])
 print_grid(grid)
+
