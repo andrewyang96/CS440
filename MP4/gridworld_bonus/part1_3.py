93bd10f1db928b56636f7d292aeccaa6777f84f5
Andrew Yang <ayang14@illinois.edu>
2015-12-03 10:22:08 -0600

    Start MP4 part 1.3 bonus

---
+++ MP4/gridworld_bonus/part1_3.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Board(object):
+    # O = empty
+    # W = wall
+    # S = student
+    # G = grocery
+    # P = pizza shop
+    def __init__(self, board, startpos):
+        # board - 2d array
+        # startpos - (x, y) coordinate
+        pass
